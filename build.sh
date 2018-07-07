b35fe0ad55b61a3d297961553522e23719c38cce
h@di <hadi@hadisafari.ir>
2018-07-07 20:18:23 +0430

    use bibtex8 for persian-bib
    http://ctan.asis.io/biblio/bibtex/contrib/persian-bib/Persian-bib-userguide.pdf#6

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 xelatex -synctex=1 MyThesis &&
-bibtex MyThesis &&
+bibtex8 -W -c cp1256fa MyThesis &&
 xindy -L persian-variant3 -C utf8 -I xindy -M MyThesis.xdy -t MyThesis.glg -o MyThesis.gls MyThesis.glo &&
 xindy -L persian-variant3 -C utf8 -I xindy -M MyThesis.xdy -t MyThesis.blg -o MyThesis.bls MyThesis.blo &&
 xindy -L english -C utf8 -I xindy -M MyThesis.xdy -t MyThesis.alg -o MyThesis.acr MyThesis.acn &&
